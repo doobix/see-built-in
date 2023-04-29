4ee628b689f70cc78f9ffa5aba27eef28514dd1f
Wesley See <doobix@gmail.com>
2023-04-29 13:43:10 -0700

    show builtin jobs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import Job from './Job';
 import './App.css';
+import builtinData from './builtin.json';
 
 function App() {
+  // const url = "https://api.builtin.com/services/job-retrieval/legacy-jobs?categories=149&subcategories=&experiences=1%2C1-3%2C3-5%2C3%2C5&industry=&company_sizes=&regions=&locations=&working_option=2&per_page=500&page=1&search=frontend&sortStrategy=recency&job_locations=&company_locations=&jobs_board=true&national=true";
+
+  // async function getData() {
+  //   const builtInData = await fetch(url).then((res) => res.json()).then(data => data);
+  //   console.log(builtInData);
+  // }
+
+  // Change companies to a map
+  const companies = builtinData.companies;
+  const companyMap = {};
+  companies.forEach((company) => {
+    companyMap[company.id] = {
+      title: company.title,
+      logo: company.logo,
+    };
+  });
+
+  // Sort jobs by date
+  const jobs = builtinData.jobs.sort((a, b) => {
+    const aDate = new Date(a.sort_job);
+    const bDate = new Date(b.sort_job);
+    if (aDate > bDate) {
+      return -1;
+    } else if (aDate < bDate) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Built In Jobs (Frontend)</h1>
+      <div>({jobs.length} jobs)</div>
+      <div className="jobs">
+        {jobs.map((job) => (
+          <Job
+            companyName={companyMap[job.company_id].title}
+            title={job.title}
+            date={job.sort_job}
+            url={job.how_to_apply}
+            logo={companyMap[job.company_id].logo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
