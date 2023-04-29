387d0318fc043d96d9b2c55e58e929fb465521ff
doobix <doobix@gmail.com>
2023-04-29 21:34:55 +0000

    Deploying to gh-pages from @ doobix/see-built-in@cc10677802038d2025ff674b86b9173ae589290d 🚀

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Built In Jobs heading', () => {
-  render(<App />);
-  const element = screen.getByText(/Built In Jobs/i);
-  expect(element).toBeInTheDocument();
-});
