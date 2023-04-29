84ff9e0c9c99ec13370e610cb8f5ac441863a373
Wesley See <doobix@gmail.com>
2023-04-29 13:51:50 -0700

    use real data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import Job from './Job';
 import './App.css';
-import builtinData from './builtin.json';
+import { useState } from 'react';
 
 function App() {
-  // const url = "https://api.builtin.com/services/job-retrieval/legacy-jobs?categories=149&subcategories=&experiences=1%2C1-3%2C3-5%2C3%2C5&industry=&company_sizes=&regions=&locations=&working_option=2&per_page=500&page=1&search=frontend&sortStrategy=recency&job_locations=&company_locations=&jobs_board=true&national=true";
-
-  // async function getData() {
-  //   const builtInData = await fetch(url).then((res) => res.json()).then(data => data);
-  //   console.log(builtInData);
-  // }
+  const [builtinData, setBuiltinData] = useState({});
+  const url = "https://api.builtin.com/services/job-retrieval/legacy-jobs?categories=149&subcategories=&experiences=1%2C1-3%2C3-5%2C3%2C5&industry=&company_sizes=&regions=&locations=&working_option=2&per_page=500&page=1&search=frontend&sortStrategy=recency&job_locations=&company_locations=&jobs_board=true&national=true";
+  async function getData() {
+    const data = await fetch(url).then((res) => res.json()).then(data => data);
+    setBuiltinData(data);
+  }
+  getData();
 
   // Change companies to a map
-  const companies = builtinData.companies;
+  const companies = builtinData.companies || [];
   const companyMap = {};
   companies.forEach((company) => {
     companyMap[company.id] = {
@@ -21,7 +22,7 @@ function App() {
   });
 
   // Sort jobs by date
-  const jobs = builtinData.jobs.sort((a, b) => {
+  const jobs = (builtinData.jobs || []).sort((a, b) => {
     const aDate = new Date(a.sort_job);
     const bDate = new Date(b.sort_job);
     if (aDate > bDate) {
@@ -38,8 +39,9 @@ function App() {
       <h1>Built In Jobs (Frontend)</h1>
       <div>({jobs.length} jobs)</div>
       <div className="jobs">
-        {jobs.map((job) => (
+        {jobs.map((job, index) => (
           <Job
+            key={index}
             companyName={companyMap[job.company_id].title}
             title={job.title}
             date={job.sort_job}
