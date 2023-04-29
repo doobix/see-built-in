137285ec9b13c24400eea6e71f5d9eb7bb66fc31
Wesley See <doobix@gmail.com>
2023-04-29 14:06:07 -0700

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Built In Jobs heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Built In Jobs/i);
+  expect(element).toBeInTheDocument();
 });
