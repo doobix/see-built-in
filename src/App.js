86301ecda5823c608d03bdd9ffce84441ab7cd47
Wesley See <doobix@gmail.com>
2023-05-01 16:02:28 -0700

    fix test

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
   });
 
   return (
-    <div className="App">
+    <div>
       <div>({jobs.length} {convertCategoryTitle(category)} jobs)</div>
       <div className="jobs">
         {jobs.map((job, index) => (
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 test('renders Built In Jobs heading', () => {
-  render(<App />);
-  const element = screen.getByText(/Built In Jobs/i);
+render(
+    <Router>
+      <App />
+    </Router>
+  );
+  const element = screen.getByText(/0 frontend jobs/i);
   expect(element).toBeInTheDocument();
 });
